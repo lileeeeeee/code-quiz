ac450454ce130f14d45bad6b0a357bd39c93ccc4
lileee <112982735+lileeeeeee@users.noreply.github.com>
2022-11-17 12:27:46 -0600

    updated questionArray to remove questions as they are answered (no longer random, however), and set innerHTML to display highscores as they accumulated from local storage. Added buttons for clearing high scores and restarting quiz. Updated button design in CSS for a more user friendly experience. Wrote README and included screenshot.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # code-quiz
 a browser app containing a multiple choice coding quiz
+
+## Description
+This is a timed quiz that keeps score while you answer questions and allows you to add your high score at the end. 
+
+## Installation
+Website is available here: https://lileeeeeee.github.io/code-quiz and looks like this:
+![screenshot](/Assets/codequizscreenshot.jpg)
+
+## Usage
+When the "Start Quiz" button is clicked, the user will be given their first multiple choice question of eight total. Once they click their answer selection, the next question is displayed along with whether or not their answer to the previous question was right or wrong (at the bottom of the card). If a wrong answer was clicked, five seconds are subtracted from the timer. Once the timer reaches zero, or all the questions have been answered the user is prompted to enter their initials. Upon submission of their initials, they will see their score, along with any previous scores. They can then clear the scores if they choose, or play again. At any point during the game, "View High Scores" is available, which will display all stored previous scores. 
+
+## Future Improvements
+Currently, the high score display for this application is very rudimentary. In the future, I would like to update how this data is pulled and displayed. 
+
+Credits
+N/A
+
+License
+See License
--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,16 @@ var answerResultDisplay = document.querySelector (".rightOrWrong");
 var questionDisplay = document.querySelector ("h2");
 var submitBtn = document.querySelector ("#submit");
 var initialInput = document.querySelector ("#initials");
+var highScoreList = document.querySelector ("#highscores");
+var clearScoresBtn = document.querySelector (".clear");
+var playAgainBtn = document.querySelector (".playagain");
 
-let questionOne = {
+var timer;
+var timerCount = 60;
+var score;
+var scoreCount = 0;
+
+var questionOne = {
 question: "what is JavaScript",
 one: "a cool font",
 two: "a dead language",
@@ -22,7 +30,7 @@ three: "a scripting language",
 four: "how a coffee would write a letter",
 rightAnswer:  "a scripting language"
 }
-let questionTwo = {
+var questionTwo = {
   question: "which of the below primitive values will not return the expected typeof when tested?",
   one: "boolean",
   two: "string",
@@ -30,7 +38,7 @@ let questionTwo = {
   four: "null",
   rightAnswer: "null"
 }
-let questionThree = {
+var questionThree = {
 question: "how long did it take to develop JavaScript?",
 one: "ten years",
 two: "ten days",
@@ -38,7 +46,7 @@ three: "ten minutes",
 four: "ten weeks",
 rightAnswer:  "ten days"
 }
-let questionFour = {
+var questionFour = {
 question: "who created JavaScript?",
 one: "Brendan Eich",
 two: "Bill Gates",
@@ -46,23 +54,39 @@ three: "Linus Torvalds",
 four: "Tim Berners-Lee",
 rightAnswer:  "Brendan Eich"
 }
-var questionArray = [questionOne, questionTwo, questionThree, questionFour];
-
-
-var timer;
-var timerCount = 400;
-var score;
-var scoreCount = 0;
-
-  // add event listener to the answer buttons, stores which button was clicked in the key 
-  btnOne.addEventListener("click", checkOne);
- 
-  btnTwo.addEventListener("click", checkTwo);
-
-  btnThree.addEventListener("click", checkThree);
-
-  btnFour.addEventListener("click", checkFour);
- 
+var questionFive = {
+  question: "each of the below are features of JavaScript, except ______",
+  one: "Compatability with other languages",
+  two: "Programming without compilation",
+  three: "Mutable and immutable data types",
+  four: "Curly brackets",
+  rightAnswer:  "Mutable and immutable data types"
+}
+var questionSix = {
+  question: "what does an event listener do?",
+  one: "attends concerts",
+  two: "adds an event to an element",
+  three: "refreshes the page",
+  four: "creates an infinite loop",
+  rightAnswer:  "adds an event to an element"
+}
+var questionSeven = {
+  question: "what does JSON stand for?",
+  one: "Java Standard Object Number",
+  two: "JavaScript Object Numerals",
+  three: "JavaScript Object Notation",
+  four: "Jason just forgot the A again",
+  rightAnswer:  "JavaScript Object Notation"
+}
+var questionEight = {
+  question: "how many basic data types are there in JavaScript?",
+  one: "eight",
+  two: "eight hundred",
+  three: "eighty",
+  four: "there is no data in JavaScript",
+  rightAnswer:  "eight"
+}
+var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight];
 
 //hides the current card and displays the lastCard  
 function displayLastCard() {
@@ -70,30 +94,39 @@ function displayLastCard() {
     questionCards.setAttribute ("style", "display: none");
     scoreInitials.setAttribute("style", "display: none");
      lastCard.setAttribute("style", "display: block");
-     //need to link this card to local storage for initials//
-     var list = localStorage.getItem("highscorelist");
-     lastCard.textContent = list;
-
-   
+     highScoreList.innerHTML = localStorage.getItem("highscores");
 }
-function collectInitialsScore () {
-  console.log(initialInput);
+
+//collects the data from the initials input text box and combines with the score from local storage into the scoresObject//
+function collectInitialsScore (event) {
+  event.preventDefault ();
   localStorage.setItem("initials", initialInput.value);
   var initials = localStorage.getItem("initials");
-  var scoreInitials = [initials, localStorage.getItem("score")];
-  console.log(scoreInitials);
-  lastCard.textContent = scoreInitials;
-  localStorage.clear("initials");
-  localStorage.clear("score");
-  localStorage.setItem("highscorelist", scoreInitials);  
-  displayLastCard();
+  var yourScore = localStorage.getItem("score");
+   var scoresObject = {
+    score: yourScore,
+    initials: initials
+     }
+     console.log(scoresObject);
+     //https://www.youtube.com/watch?v=DFhmNLKwwGw for the two lines below//
+     var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+     highScores.push(scoresObject);
+     localStorage.setItem("highscores", JSON.stringify(highScores));
+     console.log(localStorage.getItem("highscores"));
+     var scoreDisplay = localStorage.getItem("highscores");   
+     highScoreList.innerHTML = scoreDisplay;
+    
+ 
+
+  displayLastCard (); 
 }
+
 
 //hides other cards and displays initials input
 function displayInitialsInput() {
   startCard.setAttribute ("style", "display:none");
   questionCards.setAttribute ("style", "display: none");
-  lastCard.setAttribute("style", "display: block");
+  lastCard.setAttribute("style", "display: none");
   scoreInitials.setAttribute("style", "display: block");
 
 }
@@ -102,33 +135,29 @@ function displayInitialsInput() {
 function startQuestions () {
   startCard.setAttribute ("style", "display:none");
   questionCards.setAttribute ("style", "display: block");
-  
-
-    var rdmQuestion = questionArray[(([Math.floor(Math.random()*questionArray.length)]))];
-    console.log(rdmQuestion);
-    
-    //figure out how to remove that item from the array//
-    //figure out how to code if there are no items in the array, display the last card//
+  if (questionArray.length == 0) {
+    clearInterval(timer);
+    displayInitialsInput();
+    return
+  }
+    //this is no longer random but it does remove the questions from the array 
+  var rdmQuestion = questionArray.pop();
+// REMOVED [(([Math.floor(Math.random()*questionArray.length)]))]
     btnOne.textContent = rdmQuestion.one;
     btnTwo.textContent = rdmQuestion.two;
     btnThree.textContent = rdmQuestion.three;
     btnFour.textContent = rdmQuestion.four;
     questionDisplay.textContent = rdmQuestion.question;
     localStorage.setItem("answer", rdmQuestion.rightAnswer);
-    console.log(localStorage.getItem("answer"));
-  }
     
- //questionArray[(([Math.floor(Math.random()*questionArray.length)]))]; 
-
+  } 
 //checks the answer clicked, changes display message, clears local storage, reloads questions
-
 function checkOne () {
   localStorage.setItem("answer1", btnOne.textContent);
   if (localStorage.getItem("answer1") === localStorage.getItem("answer")) {
     answerResultDisplay.textContent = "Correct!";
     scoreCount = scoreCount + 1;
     localStorage.setItem("score", scoreCount);
-    console.log(localStorage.getItem("score"));
   }
   else {
     answerResultDisplay.textContent = "wrong"
@@ -138,13 +167,13 @@ function checkOne () {
   localStorage.removeItem('answer');
   startQuestions ();
 }  
+
 function checkTwo () {
   localStorage.setItem("answer2", btnTwo.textContent);
   if (localStorage.getItem("answer2") === localStorage.getItem("answer")) {
     answerResultDisplay.textContent = "Correct!";
     scoreCount = scoreCount + 1;
     localStorage.setItem("score", scoreCount);
-    console.log(localStorage.getItem("score"));
   }
   else {
     answerResultDisplay.textContent = "wrong"
@@ -154,13 +183,13 @@ function checkTwo () {
   localStorage.removeItem('answer');
   startQuestions ();
 }  
+
 function checkThree () {
   localStorage.setItem("answer3", btnThree.textContent);
   if (localStorage.getItem("answer3") === localStorage.getItem("answer")) {
     answerResultDisplay.textContent = "Correct!";
     scoreCount = scoreCount + 1;
     localStorage.setItem("score", scoreCount);
-    console.log(localStorage.getItem("score"));
   }
   else {
     answerResultDisplay.textContent = "wrong"
@@ -170,13 +199,14 @@ function checkThree () {
   localStorage.removeItem('answer');
   startQuestions ();
 }  
+
 function checkFour () {
   localStorage.setItem("answer4", btnFour.textContent);
   if (localStorage.getItem("answer4") === localStorage.getItem("answer")) {
     answerResultDisplay.textContent = "Correct!";
     scoreCount = scoreCount + 1;
     localStorage.setItem("score", scoreCount);
-    console.log(localStorage.getItem("score"));
+    
   }
   else {
     answerResultDisplay.textContent = "wrong"
@@ -193,7 +223,7 @@ function startTimer () {
       timerCount--;
       document.querySelector(".timerdisplay").textContent = timerCount;
       // Tests if time has run out
-      if (timerCount <= 0) {
+      if (timerCount == 0) {
         // Clears interval
         clearInterval(timer);
         displayInitialsInput();
@@ -204,18 +234,31 @@ function startTimer () {
 
 //starts the quiz
 function startQuiz () {
+
   startTimer();
   startQuestions ();
   
 }
+function clearScores () {
+  localStorage.removeItem("highscores");
+  displayLastCard()
+}
+function playAgain () {
+location.reload ();
+}
     
-    
-    //Add event listener to high score button
-   highScoreBtn.addEventListener("click", displayLastCard);
-  
-   //Add event listener to start button
-  startBtn.addEventListener("click", startQuiz);
-  //add event listener to the submit button
-  submitBtn.addEventListener("click", collectInitialsScore);
-   
-    
+//Add event listener to high score button
+highScoreBtn.addEventListener("click", displayLastCard);
+//Add event listener to the clear scores button
+clearScoresBtn.addEventListener("click", clearScores);
+//Add event listener to the play again button
+playAgainBtn.addEventListener("click", playAgain);
+//Add event listener to start button
+startBtn.addEventListener("click", startQuiz);
+//add event listener to the submit button
+submitBtn.addEventListener("click", collectInitialsScore); 
+// add event listener to the answer buttons, stores which button was clicked in the key 
+btnOne.addEventListener("click", checkOne);
+btnTwo.addEventListener("click", checkTwo);
+btnThree.addEventListener("click", checkThree);
+btnFour.addEventListener("click", checkFour);
